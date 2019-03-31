94debd76385f696ced6799a95355e58ef05541ea
rthunoli <rajendran.thunoli@gmail.com>
2019-03-31 12:01:54 +0400

    Add project files.

---
+++ RecoverWordFiles/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.BtnRecover = New System.Windows.Forms.Button()
+        Me.LstFiles = New System.Windows.Forms.ListBox()
+        Me.TxtPath = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.BtnBrowse = New System.Windows.Forms.Button()
+        Me.BtnPrint = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnRecover
+        '
+        Me.BtnRecover.Location = New System.Drawing.Point(114, 361)
+        Me.BtnRecover.Name = "BtnRecover"
+        Me.BtnRecover.Size = New System.Drawing.Size(75, 23)
+        Me.BtnRecover.TabIndex = 0
+        Me.BtnRecover.Text = "Recover"
+        Me.BtnRecover.UseVisualStyleBackColor = True
+        '
+        'LstFiles
+        '
+        Me.LstFiles.FormattingEnabled = True
+        Me.LstFiles.Location = New System.Drawing.Point(12, 38)
+        Me.LstFiles.Name = "LstFiles"
+        Me.LstFiles.Size = New System.Drawing.Size(372, 303)
+        Me.LstFiles.TabIndex = 1
+        '
+        'TxtPath
+        '
+        Me.TxtPath.Location = New System.Drawing.Point(12, 12)
+        Me.TxtPath.Name = "TxtPath"
+        Me.TxtPath.Size = New System.Drawing.Size(335, 20)
+        Me.TxtPath.TabIndex = 2
+        '
+        'BtnBrowse
+        '
+        Me.BtnBrowse.Location = New System.Drawing.Point(353, 10)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(31, 23)
+        Me.BtnBrowse.TabIndex = 3
+        Me.BtnBrowse.Text = "..."
+        Me.BtnBrowse.UseVisualStyleBackColor = True
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.Location = New System.Drawing.Point(213, 361)
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(75, 23)
+        Me.BtnPrint.TabIndex = 4
+        Me.BtnPrint.Text = "Print"
+        Me.BtnPrint.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(396, 396)
+        Me.Controls.Add(Me.BtnPrint)
+        Me.Controls.Add(Me.BtnBrowse)
+        Me.Controls.Add(Me.TxtPath)
+        Me.Controls.Add(Me.LstFiles)
+        Me.Controls.Add(Me.BtnRecover)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Word Documents Retriever"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnRecover As Button
+    Friend WithEvents LstFiles As ListBox
+    Friend WithEvents TxtPath As TextBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents BtnBrowse As Button
+    Friend WithEvents BtnPrint As Button
+End Class
---
+++ RecoverWordFiles/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports Word = Microsoft.Office.Interop.Word
+Imports System.Runtime.InteropServices
+Imports System
+Imports System.IO
+
+Public Class Form1
+    Dim oWord As Word.Application
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnRecover.Click
+        If (TxtPath.Text <> "") Then
+            ProcessDocs()
+        End If
+    End Sub
+
+    Private Sub PrintDocs()
+        oWord = CreateObject("Word.Application")
+        Dim oDoc As Word.Document
+        ' Make a reference to a directory.
+        Dim di As New DirectoryInfo(TxtPath.Text)
+        ' Get a reference to each file in that directory.
+        Dim fiArr As FileInfo() = di.GetFiles()
+        ' Display the names of the files.
+        Dim fri As FileInfo
+        For Each fri In fiArr
+            oDoc = oWord.Documents.Open(fri.FullName)
+            oDoc.PrintOut()
+            oDoc.Close()
+            oDoc = Nothing
+        Next fri
+        oWord.Application.Quit()
+        oWord = Nothing
+
+    End Sub
+    Private Sub ProcessDocs()
+        oWord = CreateObject("Word.Application")
+        ' Make a reference to a directory.
+        Dim di As New DirectoryInfo(TxtPath.Text)
+        ' Get a reference to each file in that directory.
+        Dim fiArr As FileInfo() = di.GetFiles()
+        ' Display the names of the files.
+        Dim fri As FileInfo
+        For Each fri In fiArr
+            Recover(fri.FullName)
+        Next fri
+        oWord.Application.Quit()
+        oWord = Nothing
+        MessageBox.Show(LstFiles.Items.Count & " files recovered successfully")
+
+    End Sub
+
+    Private Sub Recover(ByVal sFileName As String)
+        Dim oDoc As Word.Document
+
+        Try
+            Dim x = Split(sFileName, "\")
+            oDoc = oWord.Documents.Open(FileName:=sFileName, OpenAndRepair:=True)
+            With oDoc.Sections(1)
+                .Headers(Word.WdHeaderFooterIndex.wdHeaderFooterPrimary).Range.Text = x.Last()
+            End With
+            oDoc.SaveAs2(sFileName)
+            oDoc.Close()
+            oDoc = Nothing
+            LstFiles.Items.Add(x.Last())
+            LstFiles.TopIndex = LstFiles.Items.Count - 1
+            'checkedListBox1.TopIndex = checkedListBox1.Items.Count - 1;
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub BtnBrowse_Click(sender As Object, e As EventArgs) Handles BtnBrowse.Click
+        FolderBrowserDialog1.ShowDialog()
+        TxtPath.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+    Private Function isDomainUser() As Boolean
+        Return Environment.MachineName <> Environment.UserDomainName
+    End Function
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        If (isDomainUser()) Then
+            PrintDocs()
+        Else
+            MsgBox("Login as a domain user to print documents")
+        End If
+    End Sub
+End Class
